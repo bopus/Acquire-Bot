824b80ae87d1e0964cb8f35299015c6307923180
FoggyLight <foggylight527@gmail.com>
2021-04-17 00:23:55 -0700

    Sophistication 100

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import json
 import os
-import time
-
 from pathlib import Path
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
 
 if os.path.exists(os.getcwd() + "/config.json"):
     with open("./config.json") as f:
@@ -24,9 +25,10 @@ nameList = configData["Usernames"]
 
 options = webdriver.ChromeOptions()
 options.add_argument('--disable-blink-features=AutomationControlled')
-#options.add_argument("--user-data-dir={}".format(userDataDir))
+# options.add_argument("--user-data-dir={}".format(userDataDir))
 
-driver = webdriver.Chrome(executable_path=os.path.dirname(os.path.abspath(__file__)) + "/chromedriver.exe", chrome_options=options)
+driver = webdriver.Chrome(executable_path=os.path.dirname(os.path.abspath(__file__)) + "/chromedriver.exe",
+                          chrome_options=options)
 
 game_number = 0
 
@@ -34,7 +36,6 @@ game_number = 0
 def new_user(username_input, nth_user, game_number_input):
     driver.get("https://acquire.tlstyer.com/")
     login(username_input)
-    time.sleep(1)
     if nth_user % 3 == 0 or game_number_input == 0:
         return create_game()
     else:
@@ -43,7 +44,7 @@ def new_user(username_input, nth_user, game_number_input):
 
 
 def watch_game(game_number_input):
-    driver.find_element_by_xpath(".//div[@id='lobby-game-{}']//input[@value='Watch']".format(game_number_input)).click()
+    wait_for_element(By.XPATH, ".//div[@id='lobby-game-{}']//input[@value='Watch']".format(game_number_input)).click()
 
 
 def login(username_input):
@@ -54,11 +55,15 @@ def login(username_input):
 
 
 def create_game():
-    Select(driver.find_element_by_id('cg-max-players')).select_by_value("1")
+    Select(wait_for_element(By.ID, 'cg-max-players')).select_by_value("1")
     driver.find_element_by_id("button-create-game").click()
-    time.sleep(0.5)
-    for elem in driver.find_elements_by_xpath('.//span[@class = "header"]'):
-        return int(elem.text.replace(":", "").replace("Game #", ""))
+    elem = wait_for_element(By.XPATH, './/span[@class = "header"]')
+    return int(elem.text.replace(":", "").replace("Game #", ""))
+
+
+def wait_for_element(by, value):
+    print("waiting for element to load")
+    return WebDriverWait(driver, 10).until(EC.element_to_be_clickable((by, value)))
 
 
 usernames = nameList.split(",")
